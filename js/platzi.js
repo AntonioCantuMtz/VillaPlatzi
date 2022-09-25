2500a97941db54956f087a783df47009f2a48bfa
Antonio Martinez <antonio_halo1@hotmail.com>
2022-09-24 21:52:52 -0500

    Se agregaron pollos y cerdos ademas de crear un rango distinto a cada uno de los animalitos para que no aparescan la misma cantidad de animales de cada tipo cada que recargamos la pagina

--- js/platzi.js
+++ js/platzi.js
@@ -23,7 +23,9 @@ var granjero = {
 };
 
 
-var cantidad = aleatorio(2, 8);
+var cantidadDeVacas = aleatorio(2, 8);
+var cantidadDeCerdos = aleatorio(1, 5);
+var cantidadDePollos = aleatorio(4, 10);
 
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
@@ -32,6 +34,15 @@ fondo.imagen.addEventListener("load", cargarFondo);
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
 
 function cargarFondo()
 {
@@ -45,6 +56,18 @@ function cargarVacas()
     dibujar();
 }
 
+function cargarCerdos()
+{
+    cerdo.cargaOK = true;
+    dibujar();
+}
+
+function cargarPollos()
+{
+    pollo.cargaOK = true;
+    dibujar();
+}
+
 function dibujar(evento)
 {
     if(fondo.cargaOK)
@@ -53,12 +76,32 @@ function dibujar(evento)
     }
     if(vaca.cargaOK)
     {
-        console.log(cantidad);
-        for(var v = 0; v < cantidad; v++)
+        console.log("Cantidad de vacas: " +cantidadDeVacas);
+        for(var v = 0; v < cantidadDeVacas; v++)
         {
             var x = aleatorio(0, 420);
             var y = aleatorio(0, 420);
             papel.drawImage(vaca.imagen, x, y);
+        }
+    }
+    if(cerdo.cargaOK)
+    {
+        console.log("Cantidad de cerdos: " +cantidadDeCerdos);
+        for(var c = 0; c < cantidadDeCerdos; c++)
+        {
+            var x = aleatorio(0, 420);
+            var y = aleatorio(0, 420);
+            papel.drawImage(cerdo.imagen, x, y);
+        }
+    }
+    if(pollo.cargaOK)
+    {
+        console.log("Cantidad de pollos: " +cantidadDePollos);
+        for(var p = 0; p < cantidadDePollos; p++)
+        {
+            var x = aleatorio(0, 420);
+            var y = aleatorio(0, 420);
+            papel.drawImage(pollo.imagen, x, y);
         }
     }
 }
